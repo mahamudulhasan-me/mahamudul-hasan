a15f9c03e3d8e01d54e21b5c1c9ba6e68ed5f8f0
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2024-02-02 00:21:59 +0600

    add snake game on hero section

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,25 @@ import HeroSection from "@/components/HeroSection/HeroSection";
 
 export default function Home() {
   return (
-    <main className="container mx-auto px-[7%]">
+    <main className="container mx-auto px-[7%] relative">
       <HeroSection />
+      <div className="flex flex-col items-center text-slate-light fixed right-10 bottom-0">
+        <p
+          className="flex flex-col items-center text-sm"
+          style={{ transform: "rotate(-90deg)" }}
+        >
+          <span>m</span>
+          <span>a</span>
+          <span>h</span>
+          <span>@</span>
+          <span>g</span>
+          <span>m</span>
+          <span>a</span>
+          <span>i</span>
+          <span>l</span>
+        </p>
+        <div className="h-24 w-[1px] bg-slate-light"></div>
+      </div>
     </main>
   );
 }
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,22 +1,27 @@
+import SnakeGame from "./SnakeGame/SnakeGame";
+
 const HeroSection = () => {
   return (
-    <section className="min-h-[calc(100vh-80px)] flex justify-between items-center">
+    <section className="min-h-screen w-full py-10 gap-10 flex justify-between items-center">
       <aside className="w-1/2">
         <h4 className="text-green">Hi, my name is</h4>
-        <h1 className="text-5xl text-slate-lightest font-semibold">
+        <h1 className="text-5xl text-slate-lightest font-semibold mb-3 mt-4">
           Mahamudul Hasan
         </h1>
         <h1 className="text-4xl text-slate-light font-semibold">
           I build things for the web.
         </h1>
-        <p className="text-slate-light text-justify">
+        <p className="text-slate-light text-justify my-8">
           I&apos;m a software engineer specializing in building (and
           occasionally designing) exceptional digital experiences. Currently,
           I&apos;m focused on building accessible, human-centered products at
           Upstatement.
         </p>
+        <button className="btn-getResume2 mt-8">Get Resume</button>
       </aside>
-      <aside className="w-1/2"></aside>
+      <aside>
+        <SnakeGame />
+      </aside>
     </section>
   );
 };
