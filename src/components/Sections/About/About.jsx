b60e6ee6ee8cd51bd3266f6f4db7e388e9b04331
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2024-02-13 13:09:43 +0600

    update about content

--- src/components/Sections/About/About.jsx
+++ src/components/Sections/About/About.jsx
@@ -18,36 +18,50 @@ const About = () => {
       className="text-slate-400 space-y-5 leading-6 md:pt-20 md:pb-0 pb-10 text-justify md:p-4 p-0"
     >
       <MobileSectionHead icon={FcAbout} title="About" />
-      <div className="px-4 md:px0">
+      <div className="px-4 md:px-0 space-y-4">
         <p>
-          Back in 2012, I decided to try my hand at creating custom Tumblr
-          themes and tumbled head first into the rabbit hole of coding and web
-          development. Fast-forward to today, and I’ve had the privilege of
-          building software for an advertising agency, a start-up, a student-led
-          design studio, and a huge{" "}
-          <span className="hovered-text">corporation</span>.
+          As a final-year{" "}
+          <span className="hovered-text">Computer Science and Engineering</span>{" "}
+          student, my journey into the world of coding began from a place of{" "}
+          <span className="hovered-text"> passion</span>, and it has since
+          blossomed into a dedicated focus on web application development. With
+          a strong work ethic and a flair for creativity, I continually strive
+          to push the boundaries of what&apos;s possible in the digital realm.
         </p>
         <p>
-          My main focus these days is building products and leading projects for
-          our clients at Upstatement. I most enjoy building software in the
-          sweet spot where design and engineering meet — things that look good
-          but are also built well under the hood. In my free time, Ive also
-          released an online video course that covers everything you need to
-          know to build a web app with the Spotify API.
+          Currently employed as a{" "}
+          <span className="hovered-text">Frontend Developer</span> at{" "}
+          <span className="hovered-text">Sundarban Courier Service</span>,
+          I&apos;ve accumulated over a year of invaluable{" "}
+          <span className="hovered-text">industry experience</span> in this
+          field. While my primary focus lies in frontend development, I&apos;m
+          also actively exploring backend technologies to deepen my
+          understanding of the web ecosystem.
         </p>
         <p>
-          When I’m not at the computer, I’m usually rock climbing, reading,
-          hanging out with my wife and two cats, or running around Hyrule
-          searching for Korok seeds K o r o k s e e d s .
+          Beyond coding, I find solace in the pages of books and occasionally
+          pen down my thoughts. Outside the realm of technology, I revel in the
+          simplicity of nature and nurturing my garden, which serves as a source
+          of balance and inspiration in my life.
         </p>
-      </div>
-      <div className="w-3/4 pl-2  grid grid-cols-2 gap-x-4 justify-between space-y-1">
-        {skills.map((skill) => (
-          <p key={skill} className="flex items-center space-x-2 ">
-            <MdArrowRight className="text-green text-xl" />
-            <span className="hovered-text2">{skill}</span>
-          </p>
-        ))}
+        <p>
+          Here are a few of the technologies I&apos;ve been honing my skills in
+          recently:
+        </p>
+        <div className="w-3/4   grid grid-cols-2 gap-x-4 justify-between space-y-1">
+          {skills.map((skill) => (
+            <p key={skill} className="flex items-center space-x-2 ">
+              <MdArrowRight className="text-green text-xl" />
+              <span className="hovered-text2">{skill}</span>
+            </p>
+          ))}
+        </div>
+        <p>
+          Continuously seeking growth and embracing new challenges, I&apos;m
+          excited to leverage my passion and expertise to create impactful
+          solutions that resonate with users and push the boundaries of
+          what&apos;s possible in the digital landscape.
+        </p>
       </div>
     </section>
   );
