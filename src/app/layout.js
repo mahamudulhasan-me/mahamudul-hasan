024b2ccf8518a773e88290aeb16f698fb2d82aae
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2024-03-16 12:35:57 +0600

    add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -15,6 +16,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-NE3FMWGXXZ"
+        ></Script>
+        <Script id="google-analytics">
+          {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NE3FMWGXXZ');`}
+        </Script>
+      </head>
       <body className={poppins.className}>{children}</body>
     </html>
   );
