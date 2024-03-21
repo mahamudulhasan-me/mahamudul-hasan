3e9c1d35104ebef70a5923b09488af630aeb68fd
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2024-03-21 14:09:14 +0600

    update messenger chate plugin

--- src/components/FbMessenger/FbMessenger.jsx
+++ src/components/FbMessenger/FbMessenger.jsx
@@ -1,11 +1,14 @@
 "use client";
+
 import { CustomChat, FacebookProvider } from "react-facebook";
 
 const FbMessenger = () => {
+  const appId = process.env.NEXT_PUBLIC_FB_APP_ID;
+  const pageId = process.env.NEXT_PUBLIC_FB_PAGE_ID;
   return (
-    <FacebookProvider appId="363305470019931" chatSupport>
+    <FacebookProvider appId={"1083955279324134"} chatSupport>
       <CustomChat
-        pageId="625755791026936"
+        pageId={"1625755791026936"}
         minimized={false}
         themeColor="#0a192f"
       />
