3a584eca4e0727f1fe4edb3c77f41555fe159684
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2024-03-21 12:57:37 +0600

    update fb app and page id

--- src/components/FbMessenger/FbMessenger.jsx
+++ src/components/FbMessenger/FbMessenger.jsx
@@ -2,11 +2,13 @@
 import { CustomChat, FacebookProvider } from "react-facebook";
 
 const FbMessenger = () => {
-  const appId = process.env.NEXT_PUBLIC_FB_APP_ID;
-  const pageId = process.env.NEXT_PUBLIC_FB_PAGE_ID;
   return (
-    <FacebookProvider appId={appId} chatSupport>
-      <CustomChat pageId={pageId} minimized={false} themeColor="#0a192f" />
+    <FacebookProvider appId="363305470019931" chatSupport>
+      <CustomChat
+        pageId="625755791026936"
+        minimized={false}
+        themeColor="#0a192f"
+      />
     </FacebookProvider>
   );
 };
