ede492efcf68d4db72d0e833c6d73cac66c78588
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2025-06-30 20:55:08 +0600

    add biodata components and update styles; include Address, EducationalInfo, and PersonalInfo components, and integrate them into the BioData page; add Bengali font support

--- src/app/biodata/_const/personalInfo.js
+++
@@ -1,9 +0,0 @@
-export const personalInfo = [
-  { label: "Marital Status", value: "Never Married" },
-  { label: "Birth Year", value: "July, 1998" },
-  { label: "Height", value: "5′ 6″" },
-  { label: "Complexion", value: "Light Brown (উজ্জ্বল শ্যামলা)" },
-  { label: "Weight", value: "60 kg" },
-  { label: "Blood Group", value: "O+" },
-  { label: "Nationality", value: "Bangladeshi" },
-];
--- src/app/biodata/page.jsx
+++ src/app/biodata/page.jsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { GoArrowLeft } from "react-icons/go";
-import { personalInfo } from "./_const/personalInfo";
+import Address from "./_components/Address";
+import EducationalInfo from "./_components/EducationalInfo";
+import PersonalInfo from "./_components/PersonalInfo";
 const BioData = () => {
   return (
-    <div className="container mx-auto md:px-[8%] px-4 pt-20">
+    <div className="container mx-auto md:px-[8%] px-4 pt-20 font-bangla">
       <div>
         <Link
           href="/"
@@ -16,32 +18,12 @@ const BioData = () => {
           Bio-Data
         </h1>
       </div>
-      <main className="grid grid-cols-12 mt-6">
-        <aside className="col-span-4 text-slate-300 group bg-navy-light p-4 rounded-md transition-all shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] drop-shadow-lg">
-          <div className=""></div>
-          <h1 className="text-center text-2xl text-slate-100 font-medium">
-            Mahamudul Hasan
-          </h1>
-          <h1 className="text-center  text-2xl text-slate-100 font-medium">
-            মাহমুদুল হাসান
-          </h1>
-          <div className="border border-navy-lightest mt-4 rounded-lg divide-y divide-navy-lightest">
-            {personalInfo?.map((info, index) => (
-              <p
-                key={index}
-                className="grid grid-cols-2 items-center   divide-x divide-navy-lightest justify-center"
-              >
-                <span className="font-semibold px-4 py-2.5 text-center text-slate-200">
-                  {info.label}
-                </span>
-                <span className="px-4 py-2.5 text-center text-slate-300">
-                  {info.value}
-                </span>
-              </p>
-            ))}
-          </div>
+      <main className="grid grid-cols-12 mt-6 gap-8">
+        <PersonalInfo />
+        <aside className="lg:col-span-8 col-span-12 flex flex-col gap-8">
+          <Address />
+          <EducationalInfo />
         </aside>
-        <aside className="col-span-8"></aside>
       </main>
     </div>
   );
